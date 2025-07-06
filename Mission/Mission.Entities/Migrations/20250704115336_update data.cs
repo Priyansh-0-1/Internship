6fb0aefbe3f509c54155b5d32687069afe849eed
Priyansh Liya <154884246+Priyansh-0-1@users.noreply.github.com>
2025-07-06 10:36:50 +0530

    Ldce

---
+++ Mission/Mission.Entities/Migrations/20250704115336_update data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "id", "CreatedDate", "email_address", "first_name", "IsDeleted", "last_name", "ModifiedDate", "password", "phone_number", "user_image", "user_type" },
+                values: new object[] { 2, new DateTime(2020, 5, 15, 0, 0, 0, 0, DateTimeKind.Utc), "john.doe@example.com", "John", false, "Doe", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "John@123", "9123456780", "", "user" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ Mission/Mission.Entities/context/MissionDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities.context
+{
+    public class MissionDbContext : DbContext
+    {
+        public MissionDbContext(DbContextOptions<MissionDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegancyTimestampBehavior", true);
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Tatva",
+                LastName = "Admin",
+                EmailAddress = "admin@tatvasoft.com",
+                UserType = "admin",
+                Password = "Tatva@123",
+                PhoneNumber = "9876543210",
+                CreatedDate = new DateTime(2019, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+            }
+            ,
+    new User
+    {
+        Id = 2,
+        FirstName = "John",
+        LastName = "Doe",
+        EmailAddress = "john.doe@example.com",
+        UserType = "user",
+        Password = "John@123",
+        PhoneNumber = "9123456780",
+        CreatedDate = new DateTime(2020, 5, 15, 0, 0, 0, DateTimeKind.Utc)
+    } );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
